1cbb7dbd65b13cce12e1f2570bbb2acb6cfa2a80
Anna_Zagrebina <eto-projdet@yandex.ru>
2022-12-15 19:18:30 +0300

    Add Example005-ifElse

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write user name: ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "mary")
+{
+    Console.WriteLine("Hurray! It's Mary!");
+}
+else
+{
+    Console.WriteLine("Hi, " + userName + "!");
+}
