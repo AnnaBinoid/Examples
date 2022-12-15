b8ac2f9deb4475b7f43b4b505a34b0f881ce9c26
Anna_Zagrebina <eto-projdet@yandex.ru>
2022-12-15 19:09:39 +0300

    Change Example003_Sum with rand

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 3;
-int numberB = 5;
-Console.WriteLine(fnumberA + numberB);
+﻿int numberA = new Random().Next(1,150); //Случайное число от 1 до 149)
+int numberB = new Random().Next(1,150);
+Console.WriteLine ("Первое число " + numberA);
+Console.WriteLine ("Первое число " + numberB);
+Console.WriteLine("Сумма этих чисел равна " + (numberA + numberB));
